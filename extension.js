a05b27f4c51c612710b3984f234f3ed8752b406d
Mario Finelli <mario@finel.li>
2024-06-27 16:29:37 -0400

    Format code with prettier

--- extension.js
+++ extension.js
@@ -18,57 +18,66 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
-import Clutter from 'gi://Clutter';
-import GObject from 'gi://GObject';
-import St from 'gi://St';
-import GLib from 'gi://GLib';
+import Clutter from "gi://Clutter";
+import GObject from "gi://GObject";
+import St from "gi://St";
+import GLib from "gi://GLib";
 
-import {Extension, gettext as _} from 'resource:///org/gnome/shell/extensions/extension.js';
-import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
-import * as PopupMenu from 'resource:///org/gnome/shell/ui/popupMenu.js';
+import {
+  Extension,
+  gettext as _,
+} from "resource:///org/gnome/shell/extensions/extension.js";
+import * as PanelMenu from "resource:///org/gnome/shell/ui/panelMenu.js";
+import * as PopupMenu from "resource:///org/gnome/shell/ui/popupMenu.js";
 
-import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+import * as Main from "resource:///org/gnome/shell/ui/main.js";
 
 const Indicator = GObject.registerClass(
-class Indicator extends PanelMenu.Button {
-  _init() {
-    super._init(0.0, _('Headset Control'));
+  class Indicator extends PanelMenu.Button {
+    _init() {
+      super._init(0.0, _("Headset Control"));
 
-    let box = new St.BoxLayout({ vertical: false, style_class: 'panel-status-menu-box' });
-    let icon = new St.Icon({
-      icon_name: 'audio-headset-symbolic',
-      style_class: 'system-status-icon',
-    });
-    // let label = new St.Label({ text: _('%d%').format(10),
-    this.label = new St.Label({ text: '',
-            x_expand: true,
-            x_align: Clutter.ActorAlign.END,
-            y_expand: true,
-            y_align: Clutter.ActorAlign.CENTER });
+      let box = new St.BoxLayout({
+        vertical: false,
+        style_class: "panel-status-menu-box",
+      });
+      let icon = new St.Icon({
+        icon_name: "audio-headset-symbolic",
+        style_class: "system-status-icon",
+      });
+      // let label = new St.Label({ text: _('%d%').format(10),
+      this.label = new St.Label({
+        text: "",
+        x_expand: true,
+        x_align: Clutter.ActorAlign.END,
+        y_expand: true,
+        y_align: Clutter.ActorAlign.CENTER,
+      });
 
-        box.add_child(icon);
-        box.add_child(this.label);
-        this.add_child(box);
+      box.add_child(icon);
+      box.add_child(this.label);
+      this.add_child(box);
 
-    // this.label.set_text('test');
+      // this.label.set_text('test');
 
-    let item = new PopupMenu.PopupMenuItem(_('Show Notification'));
-    item.connect('activate', () => {
-      Main.notify(_('Whatʼs up, folks?'));
-    });
-    this.menu.addMenuItem(item);
+      let item = new PopupMenu.PopupMenuItem(_("Show Notification"));
+      item.connect("activate", () => {
+        Main.notify(_("Whatʼs up, folks?"));
+      });
+      this.menu.addMenuItem(item);
 
-    let that = this;
-    this.timer = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 5, () => {
-      that._getStatus();
-         return GLib.SOURCE_CONTINUE;
-    });
-  }
+      let that = this;
+      this.timer = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 5, () => {
+        that._getStatus();
+        return GLib.SOURCE_CONTINUE;
+      });
+    }
 
-  _getStatus() {
-    this.label.set_text(_('%d%').format(Math.floor(Math.random() * 100)))
-  }
-});
+    _getStatus() {
+      this.label.set_text(_("%d%").format(Math.floor(Math.random() * 100)));
+    }
+  },
+);
 
 export default class IndicatorExampleExtension extends Extension {
   enable() {
--- prefs.js
+++ prefs.js
@@ -1,33 +1,40 @@
-import Gio from 'gi://Gio';
-import Adw from 'gi://Adw';
+import Gio from "gi://Gio";
+import Adw from "gi://Adw";
 
-import {ExtensionPreferences, gettext as _} from 'resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js';
+import {
+  ExtensionPreferences,
+  gettext as _,
+} from "resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js";
 
 export default class ExamplePreferences extends ExtensionPreferences {
   fillPreferencesWindow(window) {
     // Create a preferences page, with a single group
     const page = new Adw.PreferencesPage({
-      title: _('General'),
-      icon_name: 'dialog-information-symbolic',
+      title: _("General"),
+      icon_name: "dialog-information-symbolic",
     });
     window.add(page);
 
     const group = new Adw.PreferencesGroup({
-      title: _('Appearance'),
-      description: _('Configure the appearance of the extension'),
+      title: _("Appearance"),
+      description: _("Configure the appearance of the extension"),
     });
     page.add(group);
 
     // Create a new preferences row
     const row = new Adw.SwitchRow({
-      title: _('Notify on Low Battery'),
-      subtitle: _('Whether to send a notification when the battery is low'),
+      title: _("Notify on Low Battery"),
+      subtitle: _("Whether to send a notification when the battery is low"),
     });
     group.add(row);
 
     // Create a settings object and bind the row to the `show-indicator` key
     window._settings = this.getSettings();
-    window._settings.bind('notify-low-battery', row, 'active',
-        Gio.SettingsBindFlags.DEFAULT);
+    window._settings.bind(
+      "notify-low-battery",
+      row,
+      "active",
+      Gio.SettingsBindFlags.DEFAULT,
+    );
   }
 }
